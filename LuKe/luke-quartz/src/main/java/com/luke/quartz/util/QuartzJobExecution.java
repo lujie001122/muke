6d0623f97313502617cba4cdbe4a020fe85e3ee3
LuJie <lujie001122@qq.com>
2023-02-24 20:27:39 +0800

    1.0项目初始化
    1.0项目初始化

---
+++ LuKe/luke-quartz/src/main/java/com/luke/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.luke.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.luke.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
